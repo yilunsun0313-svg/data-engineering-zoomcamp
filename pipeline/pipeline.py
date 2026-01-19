864f7ff6e1d8387b65404ef47eccd9449976d8a4
yilunsun0313-svg <yilunsun0313@gmail.com>
2026-01-19 22:35:19 +0000

    pipeline

---
+++ pipeline/pipeline.py
@@ -0,0 +1,15 @@
+import sys
+
+import pandas as pd
+
+print('arguments', sys.argv)
+
+month = int(sys.argv[1])
+
+df = pd.DataFrame({"day": [1, 2], "customers": [3, 4]})
+df['month'] = month
+print(df.head())
+
+df.to_parquet(f"output_{month}.parquet")
+
+print(f'hello pipeline, month = {month}')
