b651c6a84be872cb2937ac325a52382bf1264416
yilunsun0313@gmail.com <yilunsun0313@gmail.com>
2026-02-10 06:55:31 +0000

    added fact and dim tables and intermediate models

--- models/staging/stg_yellow_tripdata.sql
+++ models/staging/stg_yellow_tripdata.sql
@@ -13,13 +13,14 @@ select
       store_and_fwd_flag,
       cast(passenger_count as integer) as passenger_count,
       cast(trip_distance as numeric) as trip_distance,
-
+      1 as trip_type, -- yellow taxis cna only be called on street 
       -- payment info
       cast(fare_amount as numeric) as fare_amount,
       cast(extra as numeric) as extra,
       cast(mta_tax as numeric) as mta_tax,
       cast(tip_amount as numeric) as tip_amount,
       cast(tolls_amount as numeric) as tolls_amount,
+      0 as ehail_fee,
       cast(improvement_surcharge as numeric) as improvement_surcharge,
       cast(total_amount as numeric) as total_amount,
       cast(payment_type as integer) as payment_type
